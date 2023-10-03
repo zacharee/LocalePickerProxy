bd8787f73448a85352b2b0ae53d1923b0ca9e3b0
Zachary Wander <zachary.wander@gmail.com>
2023-10-03 18:01:02 -0400

    Remove testing stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,11 +65,5 @@ dependencies {
     implementation(libs.material)
     implementation(libs.coil.compose)
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
 }
